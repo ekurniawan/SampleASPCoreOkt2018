1935bf059ab02425549fadff53f223a8d3558f3f
ekurniawan <erick.kurniawan@gmail.com>
2018-10-08 11:43:35 +0700

    menambahkan IGreeter

--- SampleWebsite/Startup.cs
+++ SampleWebsite/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SampleWebsite
@@ -15,10 +16,12 @@ namespace SampleWebsite
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env,IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +30,8 @@ namespace SampleWebsite
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(
+                    greeter.GetMessageOfTheDay());
             });
         }
     }
